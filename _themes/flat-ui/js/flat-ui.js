442beb258bceaeee969ba7992c957490a45e27bd
Jason Varga <varga.jason@gmail.com>
2013-11-12 01:08:54 -0500

    css and js

---
+++ _themes/flat-ui/js/flat-ui.js
@@ -0,0 +1,9 @@
+/*! =========================================================
+ * bison-theme.flat-ui
+ * Flat UI Theme for Bison
+ * https://github.com/builtwithbison/theme.flat-ui
+ * =========================================================
+ * Copyright (c) 2013 Jason Varga
+ *
+ * This file built on 2013-11-12
+ * ========================================================= */
